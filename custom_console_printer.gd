7990d11cc0f54a6869dd566d4314b29a72478b9e
Ezra (Den) Shtanko <ezra.shtanko@gmail.com>
2026-01-10 10:49:31 -0500

    added log_stash method to custom_console_printer

--- custom_console_printer.gd
+++ custom_console_printer.gd
@@ -15,11 +15,16 @@ extends Resource
 ) var color		: String  = "lightgray"
 @export var flag_bold:	bool 			= false
 
+@export var stash_log_size		: int		= 16
+
 @export_tool_button("Print Test Message", "Node") var run_print_test_message: Callable = func () :
 	log_oh("test message")
 	log_ok("test message")
 	log_ew("test message")
 	log_fu("test message")
+
+var stash_log: Array[String] = []
+
 
 func headline() -> String:
 	var h: String = "[b]%s[/b]" % title if flag_bold else "%s" % title
@@ -29,6 +34,9 @@ func headline() -> String:
 	return "%s : " % h
 
 func log_ok(line: String) -> void:		print_rich("%s[color=lime]-V- %s[/color]" 			% [headline(), line])
-func log_oh(line: String) -> void:		print_rich("%s[color=light_gray]-i- %s[/color]" 	% [headline(), line])
-func log_ew(line: String) -> void:		print_rich("%s[color=orange]<!> %s[/color]" 		% [headline(), line])
-func log_fu(line: String) -> void:		print_rich("%s[color=crimson]=X= %s[/color]"		% [headline(), line])
+func log_oh(line: String) -> void:		print_rich("%s[color=light_gray]-i- %s[/color]" 		% [headline(), line])
+func log_ew(line: String) -> void:		print_rich("%s[color=orange]<!> %s[/color]" 			% [headline(), line])
+func log_fu(line: String) -> void:		print_rich("%s[color=crimson]=X= %s[/color]"			% [headline(), line])
+func log_stash(line: String) -> void:
+	stash_log.push_front(line)
+	while stash_log.size() > stash_log_size:	stash_log.pop_back()
